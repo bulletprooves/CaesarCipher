47e50e2a9c2a4fc0bc2a923424b9c8f2208311ce
David_Kam <dongun1115@gmail.com>
2024-03-24 03:00:45 +0900

    Initial commit

---
+++ Assets/Scripts/CaesarControlScript.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaesarControlScript : MonoBehaviour
+{
+    public GameObject outerCaesar;
+
+    [SerializeField]
+    private float rZ; // Z축으로 회전한 값
+    private int inputCount; // 입력을 몇 번 했는지 확인, 각도 수정을 위해 추가했지렁
+    private bool isAngleChangingMethodCalled; // 각도 돌아갈 때 부를 부울변수, 왜 만듦? 업데이트 함수에서 각도 바꾸는 연산이 매 프레임마다 연산되지 않게하기 위해
+    
+    private void Awake()
+    {
+        inputCount = 0; // 초기값 0
+        isAngleChangingMethodCalled = false;
+    }
+
+    // 아래 시계방향 반시계방향각각 메서드 2개는 각도 회전
+    public void Clockwise()
+    {
+        rZ = rZ - 13.84615f;
+        inputCount++;
+        isAngleChangingMethodCalled = true;
+    }
+    public void AntiClockwise()
+    {
+        rZ = rZ + 13.84615f;
+        inputCount++;
+        isAngleChangingMethodCalled = true;
+    }
+
+    // 아래 메서드는 각도 변경 메서드
+    public void ChangeAngle()
+    {
+        // 각도 변경
+        Vector3 currentAngle = transform.eulerAngles;
+        currentAngle = new Vector3(0, 0, rZ);
+        transform.eulerAngles = currentAngle;
+        // 각도 변경 임무 완료 후, 다시 부울변수 false 로
+        isAngleChangingMethodCalled = false;
+    }
+
+    private void Update()
+    {
+        // 아래 if문은 키보드용 코드
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rZ = rZ - 13.84615f;
+            inputCount++;
+            isAngleChangingMethodCalled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            rZ = rZ + 13.84615f;
+            inputCount++;
+            isAngleChangingMethodCalled = true;
+        }
+
+        // 각도 변경 메서드를 부를깝숑
+        if (isAngleChangingMethodCalled)
+            ChangeAngle();
+
+        // 입력 횟수 확인해서 각도 수정 메서드를 호출
+        if (inputCount > 26)
+            if (transform.eulerAngles.z > -0.5f && transform.eulerAngles.z < 0.5f)
+                AngleModify();
+
+        outerCaesar.transform.eulerAngles = new Vector3(0, 0, -13.84f * UIManagerScript.keyNum);
+    }
+
+    // 각도 수정 메서드
+    public void AngleModify()
+    {
+        Vector3 setAngleDegreeToZero = transform.eulerAngles;
+        setAngleDegreeToZero = new Vector3(0, 0, 0);
+        transform.eulerAngles = setAngleDegreeToZero;
+        Debug.Log("Angle Has Been Modified");
+        inputCount = 0; // 오차 각도 수정 완료되면 카운트 다시 0으로
+    }
+}
---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public static int keyNum;
+
+    public CaesarControlScript caesarControl; // 시저컨트롤스키립트파일 불러옴
+
+    public TextMeshProUGUI txtAlphabet; // Plain
+    public TextMeshProUGUI txtCiphered; // Cipher
+    public TextMeshProUGUI key; // 레퍼런스 할당 ㄱㄱ
+
+    Animator anim;
+
+    public char[] alphabet;
+    string exhibitAlpha; // 텍스트로 치환 할 거, 평문
+    string exhibitCiph_string; // 텍스트로 치환 할 거, 암호문
+    public int asciiNum;
+    bool isOpend; // 슬롯 열고닫기 부울 필드
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        keyNum = 0;
+        asciiNum = 65;
+        isOpend = false;
+        for(int i = 0; i < 26; i++)
+        {
+            // alphabet[i] = (char)(i + (int)'A'); 유니티에선 이렇게 안됨. 아래처럼 해야함.
+            alphabet[i] = System.Convert.ToChar(i + (int)'A'); // (int)'A'라 쓰던지 65라 쓰던지 맘대루
+            exhibitAlpha = alphabet[0].ToString(); // 캐릭터를 스트링으로 치환
+            txtAlphabet.text = exhibitAlpha;
+        }
+        key.text = keyNum.ToString();
+    }
+
+    // 아래 시계, 반시계 메서드는 각각 돌고돌아~ |(@~@)/ 야후~
+    public void ButtonDownClockwise()
+    {
+        caesarControl.Clockwise();
+        if (asciiNum >= 90)
+            asciiNum = 65;
+        else
+            asciiNum++;
+        ChangeText();
+    }
+
+    public void ButtonDownAntiClockwise()
+    {
+        caesarControl.AntiClockwise();
+        if (asciiNum <= 65)
+            asciiNum = 90;
+        else
+            asciiNum--;
+        ChangeText();
+    }
+
+    // 알파벳 바꾸는 메서드
+    public void ChangeText()
+    {
+        exhibitAlpha = alphabet[asciiNum - (int)'A'].ToString();
+        txtAlphabet.text = exhibitAlpha;
+    }
+
+    public void ButtonOpenSlot()
+    {
+        if (isOpend)
+        {
+            isOpend = false;
+            anim.SetBool("isOpen", false);
+        }
+        else
+        {
+            isOpend = true;
+            anim.SetBool("isOpen", true);
+        }
+    }
+
+    public void Sub()
+    {
+        if (keyNum <= 0)
+            keyNum = 25;
+        else
+            keyNum--;
+        key.text = keyNum.ToString();
+    }
+    public void Add()
+    {
+        if (keyNum >= 25)
+            keyNum = 0;
+        else
+            keyNum++;
+        key.text = keyNum.ToString();
+    }
+    // 귀찮아서 걍 밑에 때려박음 ㅎ.ㅎ
+    private void Update()
+    {
+        char cipheredCipher_char;
+        int cipherNumber = asciiNum + keyNum;
+        if (cipherNumber > 90)
+            cipherNumber -= 26;
+        cipheredCipher_char = System.Convert.ToChar(cipherNumber);
+        exhibitCiph_string = cipheredCipher_char.ToString();
+        txtCiphered.text = exhibitCiph_string;
+    }
+    public void GoToMyChannel()
+    {
+        Application.OpenURL("https://www.youtube.com/@bulletprooves");
+    }
+}
